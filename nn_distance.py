d0fe93aa32ae65dc9c2abc6182560d2744c16686
Stephen Cross <stephencross86@gmail.com>
2024-02-16 20:28:29 +0000

    Minor updates

--- nn_distance.py
+++ nn_distance.py
@@ -1,12 +1,33 @@
+# Nearest neighbour analysis for spots associated with filaments
+#
+#Written by Stephen Cross
+#
+#Feb 2024 - Imaris 7.6.5
+
+# <CustomTools>
+#   <Menu>
+#       <Submenu name="Spots Functions">
+#           <Item name="NN distance along filaments" icon="Python">
+#               <Command>PythonXT::nn_distance(%i)</Command>
+#           </Item>
+#       </Submenu>
+#   </Menu>
+#   <SurpassTab>
+#       <SurpassComponent name="nn_distance">
+#           <Item name="NN distance along filaments" icon="Python">
+#           <Command>PythonXT::nn_distance(%i)</Command>
+#       </SurpassComponent>
+#   </SurpassTab>
+# </CustomTools>
+#
+#Description:
+
+
 import ImarisLib
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-from matplotlib import animation
-from matplotlib.animation import PillowWriter
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import dijkstra
 from tqdm import tqdm
@@ -70,34 +91,6 @@ def nn_distance(imaris_id):
 
         return path
 
-    def set_axes_equal(ax):
-        """
-        Make axes of 3D plot have equal scale so that spheres appear as spheres,
-        cubes as cubes, etc.
-
-        Input
-        ax: a matplotlib axis, e.g., as output from plt.gca().
-        """
-
-        x_limits = ax.get_xlim3d()
-        y_limits = ax.get_ylim3d()
-        z_limits = ax.get_zlim3d()
-
-        x_range = abs(x_limits[1] - x_limits[0])
-        x_middle = np.mean(x_limits)
-        y_range = abs(y_limits[1] - y_limits[0])
-        y_middle = np.mean(y_limits)
-        z_range = abs(z_limits[1] - z_limits[0])
-        z_middle = np.mean(z_limits)
-
-        # The plot bounding box is a sphere in the sense of the infinity
-        # norm, hence I call half the max range the plot radius.
-        plot_radius = 0.5*max([x_range, y_range, z_range])
-
-        ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-        ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-        ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
     print("Initialising analysis")
 
     # Get Imaris instance
@@ -147,8 +140,6 @@ def nn_distance(imaris_id):
         (closest_pos_idx,closest_pos,min_dist) = find_closest_filament_position(filaments_pos=all_filaments_pos,spot_pos=spot_pos)
         all_min_dists.append(min_dist)
 
-    colours = ['orange','blue','gray']
-
     # Initialising output statistics
     out_mNames = [None]*len(all_spots_pos)
     out_mValues = [None]*len(all_spots_pos)
@@ -196,59 +187,7 @@ def nn_distance(imaris_id):
             out_mUnits[spots_idx[idx]] = 'um'
             out_mFactors[0][spots_idx[idx]] = 'Spot'
             out_mIds[spots_idx[idx]] = spots_idx[idx]
-
-            # fig = plt.figure()
-            # ax = Axes3D(fig)
-
-            # def init():
-            #     # Displaying all filaments
-            #     for plot_filament_idx in range(0,num_filaments):
-            #         plot_filaments_pos = filaments.GetPositionsXYZ(plot_filament_idx)
-            #         plot_filaments_edges = filaments.GetEdges(plot_filament_idx)
-            #         for filament_edge in plot_filaments_edges:
-            #             xx = [plot_filaments_pos[filament_edge[0]][0],plot_filaments_pos[filament_edge[1]][0]]
-            #             yy = [plot_filaments_pos[filament_edge[0]][1],plot_filaments_pos[filament_edge[1]][1]]
-            #             zz = [plot_filaments_pos[filament_edge[0]][2],plot_filaments_pos[filament_edge[1]][2]]
-            #             ax.plot(xx,yy,zz,colours[plot_filament_idx])
-
-            #     calc_path_length = 0
-            #     for i in range(0,len(path)-2):
-            #         p1 = filaments_pos[path[i]]
-            #         p2 = filaments_pos[path[i+1]]
-            #         calc_path_length = calc_path_length + get_dist(p1=p1,p2=p2)
-            #         ax.plot([p1[0],p2[0]],[p1[1],p2[1]],[p1[2],p2[2]],'green')
-
-            #     # Showing spots, their closest positions and the link between them
-            #     for i in range(0,len(spots_pos)):
-            #         # Plotting the spot position
-            #         spot_pos = spots_pos[i]
-            #         ax.scatter(spot_pos[0],spot_pos[1],spot_pos[2],marker='o',c='black')
-
-            #         # Plotting the closest filament position
-            #         closest_pos_idx = closest_pos_idxs[i]
-            #         filament_pos = filaments_pos[closest_pos_idx]
-            #         ax.scatter(filament_pos[0],filament_pos[1],filament_pos[2],marker='o',c='cyan')
-                    
-            #         # Plotting the link between the two
-            #         ax.plot([spot_pos[0],filament_pos[0]],[spot_pos[1],filament_pos[1]],[spot_pos[2],filament_pos[2]],'gray')
-
-            #     ax.scatter(filaments_pos[start_pt][0],filaments_pos[start_pt][1],filaments_pos[start_pt][2],marker='o',c='green')
-            #     ax.scatter(filaments_pos[end_pt][0],filaments_pos[end_pt][1],filaments_pos[end_pt][2],marker='o',c='red')
-                
-            #     ax.set_title('Dijkstra distance = '+str(dist_mat[end_idx,start_pt])+'\r\n'+'Calculated distance = '+str(calc_path_length))
-            #     set_axes_equal(ax)
-
-            #     return fig,
-
-            # def animate(i):
-            #     ax.view_init(elev=20., azim=i*6)
-                
-            #     return fig,
-
-            # writer = PillowWriter(fps=5)
-            # anim = animation.FuncAnimation(fig, animate, init_func=init,frames=30, interval=20, blit=True)
-            # anim.save(filaments_name+' ('+str(filament_idx)+')_'+spots_name+' ('+str(idx)+').gif', writer=writer)
-
+    
     all_spots.AddStatistics(out_mNames, out_mValues, out_mUnits, out_mFactors, out_mFactorNames, out_mIds)
 
-nn_distance(0)
+# nn_distance(0)
